ce30b1805b952fde8faba3593c71843f025827f2
ms-teacher1547 <saleh.mahamat21@facsciences-uy1.cm>
2025-07-07 23:12:26 +0100

    first commit

---
+++ src/main/java/com/uy1/mgpcalculator/MgpCalculatorUy1Application.java
@@ -0,0 +1,13 @@
+package com.uy1.mgpcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgpCalculatorUy1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgpCalculatorUy1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/uy1/mgpcalculator/MgpCalculatorUy1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uy1.mgpcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgpCalculatorUy1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
